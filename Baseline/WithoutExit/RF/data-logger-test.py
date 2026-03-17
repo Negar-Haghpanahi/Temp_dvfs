17dff331bd2f71ba2d6da9f2a2b2be6fbc0e8eca
Negar Haghpanahi <haghpanahinegar@gmail.com>
2026-03-17 01:21:01 +0000

    add test

--- Baseline/WithoutExit/RF/data-logger-test.py
+++ Baseline/WithoutExit/RF/data-logger-test.py
@@ -14,7 +14,7 @@ ina.configure()
  
 header = ["time s", "power mW"]
 
-filename = "PKL_Saved_Files/Shoaib_Power.csv"
+filename = "PKL_Saved_Files/z.csv"
 
 
 
--- Baseline/WithoutExit/RF/main_Board.py
+++ Baseline/WithoutExit/RF/main_Board.py
@@ -5,11 +5,11 @@ from sensor_control import initialize_bmi160, sensor_on, sensor_sleep
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument("--dataset_name", type=str, default="ACCGyro")
-    p.add_argument("--n_est", type=int, default=60)
-    p.add_argument("--max_depth", type=int, default=60)
+    p.add_argument("--dataset_name", type=str, default="Shoaib")
+    p.add_argument("--n_est", type=int, default=75)
+    p.add_argument("--max_depth", type=int, default=70)
     
-    p.add_argument("--fs_base", type=float, default=10.0)
+    p.add_argument("--fs_base", type=float, default=50.0)
     p.add_argument("--sensor_wakeup_sec", type=float, default=0.0)
     p.add_argument("--print_trace", action="store_true")
     
