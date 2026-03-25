f477c838f8f183fa7b5bb095856f6b66abe639d6
Haghpanahi <negar.haghpanahi@wsu.edu>
2026-03-25 09:42:11 -0700

    add config

--- SRF/Sensor-on-off/main_Board.py
+++ SRF/Sensor-on-off/main_Board.py
@@ -97,3 +97,62 @@ if __name__ =="__main__":
         write_content_to_file(f1, all_result, header)
         
     os.system("pkill -f 'python3 data_logger.py'")            
+
+
+
+  # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "PAMAP2", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=26, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=21, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=2 ,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.25, 1] ,help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[1.56], help = "Threshold combination", nargs="+")
+
+
+    # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "Epilepsy", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=87, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=22, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=3,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.37, 0.48, 1] ,help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.25, 0.37, 1],help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[1.38, 1.33], help = "Threshold combination", nargs="+")
+
+
+    # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "EMGPhysical", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=37, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=28, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=2,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.33, 1],help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[1.31], help = "Threshold combination", nargs="+")
+    
+    
+    # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "WESADchest", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=4, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=58, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=3,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.5, 0.6, 1] ,help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.44, 0.62, 1],help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[0.33, 0.82], help = "Threshold combination", nargs="+")
+    
+    # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "SelfRegulationSCP1", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=18, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=16, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=3,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.36, 0.54, 1],help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.3, 0.44, 1],help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[0.46, 0.66], help = "Threshold combination", nargs="+")
+    
+    # parser = argparse.ArgumentParser(description = "RF-H Inference")
+    # parser.add_argument("--dataset_name", type=str, default= "Shoaib", help = "The Dataset name")
+    # parser.add_argument("--n_est", type=int,default=77, help = "The number of estimators")
+    # parser.add_argument("--max_depth", type=int, default=52, help = "The max depth")
+    # parser.add_argument("--num_exits", type=int,  default=2 ,help = "The number of exits")
+    # parser.add_argument("--tree_splits", type=list, default=[0.41, 1] ,help = "Tree splits")
+    # parser.add_argument("--proportions", type=list, default=[0.25, 1] ,help = "Data proportions",  nargs="+")
+    # parser.add_argument("--th_combination", type=list, default=[1.73], help = "Threshold combination", nargs="+")
