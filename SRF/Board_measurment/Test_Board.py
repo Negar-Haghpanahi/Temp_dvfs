afc14f234a18b645555086095c668d7b24eb109c
Negar Haghpanahi <haghpanahinegar@gmail.com>
2026-03-26 21:46:24 +0000

    add epil+scp+emg

--- SRF/Board_measurment/Test_Board.py
+++ SRF/Board_measurment/Test_Board.py
@@ -2,37 +2,18 @@ from Inference_Board import RunInference
 import time
 
 
-def full_window_time_sec(window_len, fs_base):
-    return float(window_len) / float(fs_base)
 
 
-def stage_acquisition_times(split_points, window_len, fs_base):
-  
-    T_window = full_window_time_sec(window_len, fs_base)
-    prev = 0.0
-    out = []
-    for p in split_points:
-        p = float(p)
-        seg_prop = max(0.0, p - prev)
-        out.append(seg_prop * T_window)
-        prev = p
-    return out
-
-
-def Test(X_test , y_test , models ,args,  fs_base , window_len ,split_points ):
+def Test(X_test , y_test , models ,args ):
     
     all_per_sample_results = []
     
 
 
-    T_window = full_window_time_sec(window_len, fs_base)
-    acq_times = stage_acquisition_times(split_points, window_len, fs_base)
     
     # Now, loop through each data type for inference and add type-specific metrics
     for w in range(len(X_test)):
         
-        sensor_total_on_sec = 0.0
-        compute_total_sec = 0.0
         
         start_time_infernce = time.time()
         inference_obj = RunInference(X_test=X_test[w], y_test=y_test[w], models=models, stages=args.proportions , window_num= w)
--- SRF/Board_measurment/main_Board.py
+++ SRF/Board_measurment/main_Board.py
@@ -6,26 +6,29 @@ from Test_Board import Test
 
 def parse_args():
    
-    parser = argparse.ArgumentParser(description = "RF-H Inference")
-    parser.add_argument("--dataset_name", type=str, default= "Epilepsy", help = "The Dataset name")
-    parser.add_argument("--n_est", type=int,default=87, help = "The number of estimators")
-    parser.add_argument("--max_depth", type=int, default=22, help = "The max depth")
-    parser.add_argument("--num_exits", type=int,  default=3,help = "The number of exits")
-    parser.add_argument("--tree_splits", type=list, default=[0.37, 0.48, 1] ,help = "Tree splits")
-    parser.add_argument("--proportions", type=list, default=[0.25, 0.37, 1],help = "Data proportions",  nargs="+")
-    parser.add_argument("--th_combination", type=list, default=[1.38, 1.33], help = "Threshold combination", nargs="+")
-
-
-    # parser = argparse.ArgumentParser(description = "RF-H Inference")
-    # parser.add_argument("--dataset_name", type=str, default= "PAMAP2", help = "The Dataset name")
-    # parser.add_argument("--n_est", type=int,default=26, help = "The number of estimators")
-    # parser.add_argument("--max_depth", type=int, default=21, help = "The max depth")
-    # parser.add_argument("--num_exits", type=int,  default=2 ,help = "The number of exits")
-    # parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
-    # parser.add_argument("--proportions", type=list, default=[0.25, 1] ,help = "Data proportions",  nargs="+")
-    # parser.add_argument("--th_combination", type=list, default=[1.56], help = "Threshold combination", nargs="+")
+     parser = argparse.ArgumentParser(description = "RF-H Inference")
+     parser.add_argument("--dataset_name", type=str, default= "EMGPhysical", help = "The Dataset name")
+     parser.add_argument("--n_est", type=int,default=37, help = "The number of estimators")
+     parser.add_argument("--max_depth", type=int, default=28, help = "The max depth")
+     parser.add_argument("--num_exits", type=int,  default=2,help = "The number of exits")
+     parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
+     parser.add_argument("--proportions", type=list, default=[0.33, 1],help = "Data proportions",  nargs="+")
+     parser.add_argument("--th_combination", type=list, default=[1.31], help = "Threshold combination", nargs="+")
     
-    return parser.parse_args()
+
+     #parser = argparse.ArgumentParser(description = "RF-H Inference")
+     #parser.add_argument("--dataset_name", type=str, default= "PAMAP2", help = "The Dataset name")
+     #parser.add_argument("--n_est", type=int,default=26, help = "The number of estimators")
+     #parser.add_argument("--max_depth", type=int, default=21, help = "The max depth")
+     #parser.add_argument("--num_exits", type=int,  default=2 ,help = "The number of exits")
+     #parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
+     #parser.add_argument("--proportions", type=list, default=[0.25, 1] ,help = "Data proportions",  nargs="+")
+     #parser.add_argument("--th_combination", type=list, default=[1.56], help = "Threshold combination", nargs="+")
+    
+
+    
+
+     return parser.parse_args()
 
 def write_content_to_file(file, content, header): # the content is a list of dictionaries
     writer = csv.writer(file)
@@ -40,8 +43,9 @@ def add_header(file, header):
     
     
 if __name__ =="__main__":
-    
-    time.sleep(2)
+    print("t1 is -->",time.time())
+    time.sleep(5)
+    print("t2  is -->",time.time())
     args = parse_args()
     classData = LoadData()
     classData.Read(args.dataset_name)
