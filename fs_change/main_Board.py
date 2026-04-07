871f20f9ad77feb825f419a8e0cbb5e819c69260
Negar Haghpanahi <haghpanahinegar@gmail.com>
2026-04-07 23:36:33 +0100

    epi-fs

--- fs_change/main_Board.py
+++ fs_change/main_Board.py
@@ -9,19 +9,16 @@ from sensor_control import   set_odr_Acc, set_sensor_off  , init_sensor #initial
 def parse_args():
 
          p = argparse.ArgumentParser()
-         p.add_argument("--dataset_name", type=str, default="Shoaib")
-         p.add_argument("--n_est", type=int, default=75)
-         p.add_argument("--max_depth", type=int, default=70)
+         p.add_argument("--dataset_name", type=str, default="Epilepsy")
+         p.add_argument("--n_est", type=int, default=80)
+         p.add_argument("--max_depth", type=int, default=20)
          p.add_argument("--num_exits", type=int, default=3)
 
          p.add_argument("--tree_splits", type=float, nargs="+", default=[0.31, 0.54, 1])
          p.add_argument("--proportions", type=float, nargs="+", default=[0.39, 0.57, 1])   # split_points
-         p.add_argument("--th_combination", type=float, nargs="+", default=[0.48647753726382825, 1.945910149055313])    # th_list
+         p.add_argument("--th_combination", type=float, nargs="+", default=[0.34657359027997264, 1.3862943611198906])    # th_list
 
-
-
-
-         p.add_argument("--fs_base", type=float, default=50.0)
+         p.add_argument("--fs_base", type=float, default=250.0)
          p.add_argument("--sensor_wakeup_sec", type=float, default=0.0)
          p.add_argument("--print_trace", action="store_true")
          return p.parse_args()
@@ -88,7 +85,7 @@ if __name__ =="__main__":
 
     output_file = (
         f"PKL_Saved_Files/margin1.5/RF/"
-        f"{args.dataset_name}_accuracy_results_test.csv"
+        f"{args.dataset_name}_accuracy_results.csv"
     )
 
     num_exits = len(model.split_points)
