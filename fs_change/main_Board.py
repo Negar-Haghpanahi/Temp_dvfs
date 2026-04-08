0be8dbb05c88e31e5b95b87262e1339fc03dfdfd
Negar Haghpanahi <haghpanahinegar@gmail.com>
2026-04-08 23:35:34 +0100

    add wisdm gb

--- fs_change/main_Board.py
+++ fs_change/main_Board.py
@@ -7,19 +7,19 @@ from sensor_control import   set_odr_Acc, set_sensor_off  , init_sensor #initial
 
 
 def parse_args():
-         p = argparse.ArgumentParser()    
-         p.add_argument("--dataset_name", type=str, default="ACCGyro")
-         p.add_argument("--n_est", type=int, default=150)
-         p.add_argument("--max_depth", type=int, default=4)
-         p.add_argument("--num_exits", type=int, default=4)
-         p.add_argument("--tree_splits", type=float, nargs="+", default=[0.32, 0.47 ,0.59, 1])
-         p.add_argument("--proportions", type=float, nargs="+", default=[0.3, 0.41 ,0.56, 1])   # split_points
-         p.add_argument("--th_combination", type=float, nargs="+", default=[0.6931, 0.1732, 0.34657])
- 
-         p.add_argument("--fs_base", type=float, default=10.0)
-         p.add_argument("--sensor_wakeup_sec", type=float, default=0.0)
-         p.add_argument("--print_trace", action="store_true")
-         return p.parse_args()
+        p = argparse.ArgumentParser()    
+        p.add_argument("--dataset_name", type=str, default="wharDataOriginal")
+        p.add_argument("--n_est", type=int, default=120)
+        p.add_argument("--max_depth", type=int, default=5)
+        p.add_argument("--num_exits", type=int, default=4)
+
+        p.add_argument("--tree_splits", type=float, nargs="+", default=[0.32, 0.48, 0.59, 1])
+        p.add_argument("--proportions", type=float, nargs="+", default=[0.35, 0.47, 0.61, 1])   # split_points
+        p.add_argument("--th_combination", type=float, nargs="+", default=[0.5198603, 1.559581,1.03972])
+        p.add_argument("--fs_base", type=float, default=20.0)
+        p.add_argument("--sensor_wakeup_sec", type=float, default=0.0)
+        p.add_argument("--print_trace", action="store_true")
+        return p.parse_args()
      
     
 
