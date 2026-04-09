e5e48bf174e5dcc9cebec99c2f6588b810ea3c5c
Negar Haghpanahi <haghpanahinegar@gmail.com>
2026-04-09 01:54:08 +0100

    all rf/fs

--- fs_change/data-logger.py
+++ fs_change/data-logger.py
@@ -14,7 +14,7 @@ ina.configure()
  
 header = ["time s", "power mW"]
 
-filename = "PKL_Saved_Files/margin1.5/GB/Shoaib_Power.csv"
+filename = "PKL_Saved_Files/margin1.5/RF/Epilepsy_Power.csv"
 
 data_all = []
  
--- fs_change/main_Board.py
+++ fs_change/main_Board.py
@@ -8,17 +8,15 @@ from sensor_control import   set_odr_Acc, set_sensor_off  , init_sensor #initial
 
 def parse_args():
      p = argparse.ArgumentParser()    
-     p.add_argument("--dataset_name", type=str, default="Shoaib")
-     p.add_argument("--n_est", type=int, default=200)
-     p.add_argument("--max_depth", type=int, default=5)
-     p.add_argument("--num_exits", type=int, default=4)
-
-     p.add_argument("--tree_splits", type=float, nargs="+", default=[0.32, 0.48, 0.59, 1])
-     p.add_argument("--proportions", type=float, nargs="+", default=[0.35, 0.47, 0.61, 1])   # split_points
-     p.add_argument("--th_combination", type=float, nargs="+", default=[0.4864, 1.45943, 0.9729550])    # th_list
+     p.add_argument("--dataset_name", type=str, default="Epilepsy")
+     p.add_argument("--n_est", type=int, default=80)
+     p.add_argument("--max_depth", type=int, default=20)
+     p.add_argument("--num_exits", type=int, default=3)
+     p.add_argument("--tree_splits", type=float, nargs="+", default=[0.31, 0.54, 1])
+     p.add_argument("--proportions", type=float, nargs="+", default=[0.39, 0.57, 1])   # split_points
+     p.add_argument("--th_combination", type=float, nargs="+", default=[0.34657359027997264, 1.3862943611198906])    # th_list
       
-
-     p.add_argument("--fs_base", type=float, default=50.0)
+     p.add_argument("--fs_base", type=float, default=250.0)
      p.add_argument("--sensor_wakeup_sec", type=float, default=0.0)
      p.add_argument("--print_trace", action="store_true")
      return p.parse_args()
@@ -49,12 +47,12 @@ if __name__ =="__main__":
     classData.SplitData()
 
     
-    with open(f"PKL_Saved_Files/margin1.5/GB/{args.dataset_name}_trained_model.pkl", "rb") as f:
+    with open(f"PKL_Saved_Files/margin1.5/RF/{args.dataset_name}_trained_model.pkl", "rb") as f:
         all_models = pickle.load(f,encoding='latin1')
         
    
-    x_path = f"PKL_Saved_Files/margin1.5/GB/{args.dataset_name}_X_test.npy"
-    y_path = f"PKL_Saved_Files/margin1.5/GB/{args.dataset_name}_y_test.npy"
+    x_path = f"PKL_Saved_Files/margin1.5/RF/{args.dataset_name}_X_test.npy"
+    y_path = f"PKL_Saved_Files/margin1.5/RF/{args.dataset_name}_y_test.npy"
 
     if os.path.exists(x_path) and os.path.exists(y_path):
         X_test = np.load(x_path, allow_pickle=True)
@@ -84,7 +82,7 @@ if __name__ =="__main__":
     all_result = TestBoardControlled(X_test=X_test,y_test=y_test,model=model,args=args,sensor_on=set_odr_Acc ,sensor_sleep=set_sensor_off,fs_base=args.fs_base,window_len=window_len,sensor_wakeup_sec=0.0,print_trace=args.print_trace,)
 
     output_file = (
-        f"PKL_Saved_Files/margin1.5/GB/"
+        f"PKL_Saved_Files/margin1.5/RF/"
         f"{args.dataset_name}_accuracy_results.csv"
     )
 
