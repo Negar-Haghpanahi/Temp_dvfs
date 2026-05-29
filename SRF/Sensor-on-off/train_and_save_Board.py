5f94a117f4ebc49fc546ae0864f1534c41bdf6a5
Haghpanahi <negar.haghpanahi@wsu.edu>
2026-05-29 14:46:40 -0700

    new pkl

--- SRF/Sensor-on-off/train_and_save_Board.py
+++ SRF/Sensor-on-off/train_and_save_Board.py
@@ -96,13 +96,14 @@ def run_staged_srf(X_train, y_train, X_VAL, y_VAL, config_index,
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "RF-H Inference")
-    parser.add_argument("--dataset_name", type=str, default= "EMGPhysical", help = "The Dataset name")
-    parser.add_argument("--n_est", type=int,default=37, help = "The number of estimators")
-    parser.add_argument("--max_depth", type=int, default=28, help = "The max depth")
-    parser.add_argument("--num_exits", type=int,  default=2,help = "The number of exits")
-    parser.add_argument("--tree_splits", type=list, default=[0.5, 1] ,help = "Tree splits")
-    parser.add_argument("--proportions", type=list, default=[0.33, 1],help = "Data proportions",  nargs="+")
-    parser.add_argument("--th_combination", type=list, default=[1.31], help = "Threshold combination", nargs="+")
+    
+    parser.add_argument("--dataset_name", type=str, default= "Epilepsy", help = "The Dataset name")
+    parser.add_argument("--n_est", type=int,default=87, help = "The number of estimators")
+    parser.add_argument("--max_depth", type=int, default=22, help = "The max depth")
+    parser.add_argument("--num_exits", type=int,  default=3,help = "The number of exits")
+    parser.add_argument("--tree_splits", type=list, default=[0.37, 0.48, 1] ,help = "Tree splits")
+    parser.add_argument("--proportions", type=list, default=[0.25, 0.37, 1],help = "Data proportions",  nargs="+")
+    parser.add_argument("--th_combination", type=list, default=[1.38, 1.33], help = "Threshold combination", nargs="+")
     
     return parser.parse_args()
 
